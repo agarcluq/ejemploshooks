a7c21772509f5052bf12d12a28151d27b8606588
agarcluq <agarcluq@everis.com>
2021-04-27 14:03:07 +0200

    toDo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React, { useState } from "react";
+import React, {  useState, useEffect, useRef } from "react";
 import "./style.css";
+import { ProgressBar } from 'primereact/progressbar';
 
 export default function App() {
-  const word='patata';
-  const winBoolean=false;
-  const [win,seTwin]=useState(false)
-  const [countPoint,setCountPoint]=useState(0);
+
+  //Tenemos un array donde va guardar las tareas, este nos devuleve
+  // un array de dos pposiciones, posición y la posición para actualizar el estado
+  const [todos,addTodo]=useState([])
+
+  function setTodo(todo){
+
+    addTodo([...todos,todo]);
+  }
+  function deleteTodo(todo){
+    var index=todos.indexOf(todo);
+    todos.splice(index,1);
+    addTodo([...todos])
+  }
+
+  useEffect(()=>{
+    console.log()
+  })
   return (
-    <div>
-      <h1>{countPoint}</h1>
-      <p>
-      {win ? 'Ganador' :'Perdedor'}</p>
-      <input type="text" name="letra" id="letra"/>
-      <button onClick={()=>{
-        setCountPoint(countPoint+1)
-      }}>Enviar</button>
+    <div className="App">
+      <input type="text" id="input-todo"/>
+      <button 
+      onClick={() => {setTodo(document.getElementById("input-todo").value)}}>Añadir tarea</button>
+      <ul>
+      {todos.map(todo =>(
+        <div className="todo">
+          <li key={todo}>{todo}</li>
+          <p onClick={() =>deleteTodo(todo)}>Eliminar</p>
+          
+        </div>
+        ))}
+      </ul>
 
     </div>
   );
